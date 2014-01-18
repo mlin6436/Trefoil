befaa489d41e1f28ca0cb6bbd4504235b8cbfd89
mlin6436 <mlin6436@gmail.com>
2014-01-18 15:43:14 +0000

    setup test framework

---
+++ Trefoil.Service.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trefoil.Service.Tests
+{
+    public class Class1
+    {
+    }
+}
